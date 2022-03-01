8f60b7cd914e1e41a8323d9077883ed462f0d264
saci zakaria <sacideveloppeur@gmail.com>
2022-03-01 11:02:27 +0100

    Screen Text

--- lib/modules/shop_app/on_boarding/on_boarding_screen.dart
+++ lib/modules/shop_app/on_boarding/on_boarding_screen.dart
@@ -9,9 +9,25 @@ class OnBoardingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('OnBoardingScreen'),
       ),
-      body:Column(
+      body: Column(
+
         children: const [
-          Image(image: AssetImage('assets/images/onboard_1.jpg'))
+          Image(
+            image: AssetImage('assets/images/onboard_1.jpg'),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Text(
+            'Screen Title}',
+            style: TextStyle(
+              fontSize: 24.0,
+            ),
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+
         ],
       ),
     );
