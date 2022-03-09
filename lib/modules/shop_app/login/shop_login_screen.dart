011d455fa221b9b5c43e16e952d353b1f0de498b
saci zakaria <sacideveloppeur@gmail.com>
2022-03-09 17:48:35 +0100

    style

--- lib/modules/shop_app/login/shop_login_screen.dart
+++ lib/modules/shop_app/login/shop_login_screen.dart
@@ -1,14 +1,22 @@
-
-
 import 'package:flutter/material.dart';
 
-class ShopLoginScreen  extends StatelessWidget {
-  const ShopLoginScreen ({Key? key}) : super(key: key);
+class ShopLoginScreen extends StatelessWidget {
+  const ShopLoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Column(
+        children: [
+          Text(
+            'LOGIN',
+            style: Theme.of(context).textTheme.headline4?.copyWith(
+              color: Colors.black
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
