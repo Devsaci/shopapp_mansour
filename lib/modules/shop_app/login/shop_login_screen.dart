f36d19b0b6b167dedb647d587f7b25f782e84b52
saci zakaria <sacideveloppeur@gmail.com>
2022-03-09 18:04:20 +0100

    Padding

--- lib/modules/shop_app/login/shop_login_screen.dart
+++ lib/modules/shop_app/login/shop_login_screen.dart
@@ -7,26 +7,27 @@ class ShopLoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-        Text(
-        'LOGIN',
-        style: Theme
-            .of(context)
-            .textTheme
-            .headline4
-            ?.copyWith(color: Colors.black),
-      ),
-      Text(
-        'Login now to browse our hot offers',
-        style: Theme
-            .of(context)
-            .textTheme
-            .headline4
-            ?.copyWith(
-            color: Colors.black
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'LOGIN',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  ?.copyWith(color: Colors.black),
+            ),
+            Text(
+              'Login now to browse our hot offers',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1
+                  ?.copyWith(color: Colors.black),
+            ),
+          ],
         ),
-        ],
       ),
     );
   }
