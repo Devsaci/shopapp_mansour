3846f13ecafdd02d7f1455ba2d7153a8ea782736
saci zakaria <sacideveloppeur@gmail.com>
2022-03-10 11:34:47 +0100

    Widget defaultButton

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -159,3 +159,31 @@ void navigateAndFinish(
         return false;
       },
     );
+
+Widget defaultButton({
+  double width = double.infinity,
+  Color background = Colors.blue,
+  bool isUpperCase = true,
+  double radius = 3.0,
+  required Function() function,
+  required String text,
+}) =>
+    Container(
+      width: width,
+      height: 50.0,
+      child: MaterialButton(
+        onPressed: function,
+        child: Text(
+          isUpperCase ? text.toUpperCase() : text,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          radius,
+        ),
+        color: background,
+      ),
+    );
