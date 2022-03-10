d15a61e96a5640bdb7fc0fe56b4eede232efb205
saci zakaria <sacideveloppeur@gmail.com>
2022-03-10 18:05:26 +0100

    Widget defaultTextButton()

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 
 import '../../modules/news_app/web_view/web_view_screen.dart';
+
+
+
+Widget defaultTextButton() {
+  return Text('data');
+}
 
 Widget defaultFormField({
   TextEditingController? controller,
