b62a3f5dfbb14077abe6cd1e03eb2fb4c07b220c
saci zakaria <sacideveloppeur@gmail.com>
2022-03-11 15:48:02 +0100

    ShopLoginCubit get(context)

--- lib/modules/shop_app/login/cubit/cubit.dart
+++ lib/modules/shop_app/login/cubit/cubit.dart
@@ -1,8 +1,11 @@
 import 'package:app_theme_mansour/modules/shop_app/login/cubit/states.dart';
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class  ShopLoginCubit extends Cubit<ShopLoginStates>{
   // ShopLoginCubit(ShopLoginStates initialState) : super(initialState);
   ShopLoginCubit() : super(ShopLoginInitialState());
+
+  static ShopLoginCubit get(context) => BlocProvider.of(context);
 
 }
