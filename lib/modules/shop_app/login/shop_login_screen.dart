ac805b2df5195e4ba08ba840497b376c56367a55
saci zakaria <sacideveloppeur@gmail.com>
2022-03-14 10:25:10 +0100

    BlocConsumer<ShopLoginCubit, ShopLoginStates>

--- lib/modules/shop_app/login/shop_login_screen.dart
+++ lib/modules/shop_app/login/shop_login_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:app_theme_mansour/shared/components/components.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,9 +5,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../register/shop_register_screen.dart';
 import 'cubit/cubit.dart';
+import 'cubit/states.dart';
 
 class ShopLoginScreen extends StatelessWidget {
-   ShopLoginScreen({Key? key}) : super(key: key);
+  ShopLoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,95 +17,98 @@ class ShopLoginScreen extends StatelessWidget {
 
     return BlocProvider(
       create: (BuildContext context) => ShopLoginCubit(),
-      child: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'LOGIN',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline4
-                        ?.copyWith(color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Text(
-                    'Login now to browse our hot offers',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText2
-                        ?.copyWith(color: Colors.black),
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  defaultFormField(
-                    controller: emailController,
-                    type: TextInputType.emailAddress,
-                    validate: (value) {
-                      if (value!.isEmpty) {
-                        return 'please enter your email address';
-                      }
-                    },
-                    label: 'Email Address',
-                    prefix: Icons.email_outlined,
-                  ),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  defaultFormField(
-                    controller: passwordController,
-                    type: TextInputType.visiblePassword,
-                    suffix: Icons.visibility,
-                    suffixPressed: () {},
-                    validate: (value) {
-                      if (value!.isEmpty) {
-                        return 'password is too short';
-                      }
-                    },
-                    label: 'Password',
-                    prefix: Icons.lock_outline,
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  defaultButton(
-                    function: () {},
-                    text: 'login',
-                    isUpperCase: true,
-                  ),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+      child: BlocConsumer<ShopLoginCubit, ShopLoginStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text(
-                        'Don\'t have an account?',
+                      Text(
+                        'LOGIN',
+                        style: Theme.of(context).textTheme.headline4?.copyWith(
+                            color: Colors.black, fontWeight: FontWeight.bold),
                       ),
-                      defaultTextButton(
-                        function: () {
-                          navigateTo(
-                            context,
-                            ShopRegisterScreen(),
-                          );
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Text(
+                        'Login now to browse our hot offers',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2
+                            ?.copyWith(color: Colors.black),
+                      ),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      defaultFormField(
+                        controller: emailController,
+                        type: TextInputType.emailAddress,
+                        validate: (value) {
+                          if (value!.isEmpty) {
+                            return 'please enter your email address';
+                          }
                         },
-                        text: ('register'),
+                        label: 'Email Address',
+                        prefix: Icons.email_outlined,
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      defaultFormField(
+                        controller: passwordController,
+                        type: TextInputType.visiblePassword,
+                        suffix: Icons.visibility,
+                        suffixPressed: () {},
+                        validate: (value) {
+                          if (value!.isEmpty) {
+                            return 'password is too short';
+                          }
+                        },
+                        label: 'Password',
+                        prefix: Icons.lock_outline,
+                      ),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      defaultButton(
+                        function: () {},
+                        text: 'login',
+                        isUpperCase: true,
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account?',
+                          ),
+                          defaultTextButton(
+                            function: () {
+                              navigateTo(
+                                context,
+                                ShopRegisterScreen(),
+                              );
+                            },
+                            text: ('register'),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
