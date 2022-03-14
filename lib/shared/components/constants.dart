b3276c781837430eda4a5274ef9cf144c6b6aaea
saci zakaria <sacideveloppeur@gmail.com>
2022-03-14 18:48:42 +0100

    status: true, message: تم تسجيل الدخول بنجاح, data:

--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -8,3 +8,36 @@
 // method (url) : v2/top-headlines?
 // queries : country=eg&category=business&apiKey=ea0f2b208b944b08ab554dc5e9f5505f
 // https://newsapi.org/v2/everything?q=tesla&apiKey=ea0f2b208b944b08ab554dc5e9f5505f
+
+
+//https://www.getpostman.com/collections/94db931dc503afd508a5 first option
+//https://www.getpostman.com/collections/3223d639447a8524e38f second option
+// {
+// "status": true,
+// "message": "تم تسجيل الدخول بنجاح",
+// "data": {
+// "id": 12820,
+// "name": "saci1 zakaria",
+// "email": "saci1@gmail.com",
+// "phone": "111112",
+// "image": "https://student.valuxapps.com/storage/assets/defaults/user.jpg",
+// "points": 0,
+// "credit": 0,
+// "token": "PZ4aAGOvROPiCbGZ7dYRmXSR0GfmTOviIB1IKzsyjaal3mFKFaZE7gHob4LcOpoIobxpp5"
+// }
+// }
+//
+// {
+// "status": true,
+// "message": "تم تسجيل الدخول بنجاح",
+// "data": {
+// "id": 12820,
+// "name": "saci1 zakaria",
+// "email": "saci1@gmail.com",
+// "phone": "111112",
+// "image": "https://student.valuxapps.com/storage/assets/defaults/user.jpg",
+// "points": 0,
+// "credit": 0,
+// "token": "8aU6GtBrzqRXkt6PcaSWHy0AxDvyLcB9SfLBlEYvLjJXmoR2swiBlGAhf7NTGQG01bp4ks"
+// }
+// }
