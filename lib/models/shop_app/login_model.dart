b692562da602b2356a148cdcd9d61761172f9ca9
saci zakaria <sacideveloppeur@gmail.com>
2022-03-15 18:55:15 +0100

    class UserData attributes

--- lib/models/shop_app/login_model.dart
+++ lib/models/shop_app/login_model.dart
@@ -5,6 +5,13 @@ class ShopLoginModel {
 }
 
 class UserData {
-
+  late int id;
+  late String name;
+  late String email;
+  late String phone;
+  late String image;
+  late int points;
+  late int credit;
+  late String token;
 
 }
