b086acf60d02305a8a2694dc0e02861cbdfe9c1d
saci zakaria <sacideveloppeur@gmail.com>
2022-03-16 10:11:36 +0100

    UserData.fromJson(Map<String, dynamic> json)

--- lib/models/shop_app/login_model.dart
+++ lib/models/shop_app/login_model.dart
@@ -24,7 +24,16 @@ class UserData {
     required this.credit,
     required this.token,
   });
+
 // named constructor
-  UserData.fromJson(this.id, this.name, this.email, this.phone, this.image,
-      this.points, this.credit, this.token);
+  UserData.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+    image = json['image'];
+    points = json['points'];
+    credit = json['credit'];
+    token = json['token'];
+  }
 }
