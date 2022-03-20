a0951563d74f0c282d0d0d17b8d04aef9e725ff5
saci zakaria <sacideveloppeur@gmail.com>
2022-03-20 13:50:05 +0100

    chooseToastColor(state),

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -207,7 +207,7 @@ void showToast({
       toastLength: Toast.LENGTH_LONG,
       gravity: ToastGravity.BOTTOM,
       timeInSecForIosWeb: 5,
-      backgroundColor: Colors.red,
+      backgroundColor: chooseToastColor(state),
       textColor: Colors.white,
       fontSize: 16.0,
     );
