83a0aacedcdb16cfa565ecb0c2e7970975d4b324
saci zakaria <sacideveloppeur@gmail.com>
2022-03-20 19:05:27 +0100

    navigateAndFinish(context, ShopLoginScreen());

--- lib/layout/shop_app/shop_layout.dart
+++ lib/layout/shop_app/shop_layout.dart
@@ -1,5 +1,7 @@
 import 'dart:html';
 
+import 'package:app_theme_mansour/modules/shop_app/login/shop_login_screen.dart';
+import 'package:app_theme_mansour/shared/components/components.dart';
 import 'package:app_theme_mansour/shared/network/local/cache_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -13,8 +15,14 @@ class ShopLayout extends StatelessWidget {
         title: Text('Salla'),
       ),
       body: TextButton(
-        onPressed: (){
-          CacheHelper.removeData(key: 'token').then((value) => null);
+        onPressed: () {
+          CacheHelper.removeData(
+            key: 'token',
+          ).then((value) {
+            if(value){
+              navigateAndFinish(context, ShopLoginScreen());
+            }
+          });
         },
         child: Text('SING OUT'),
       ),
