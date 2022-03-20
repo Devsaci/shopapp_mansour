3408b82d2bbb1de2e318d05b47e2e7c4dfe505c2
saci zakaria <sacideveloppeur@gmail.com>
2022-03-20 10:52:39 +0100

    value is bool

--- lib/shared/network/local/cache_helper.dart
+++ lib/shared/network/local/cache_helper.dart
@@ -24,6 +24,7 @@ class CacheHelper {
   }) async {
     if (value is String) return await sharedPreferences.setString(key, value);
     if (value is int) return await sharedPreferences.setInt(key, value);
+    if (value is bool) return await sharedPreferences.setBool(key, value);
     return await sharedPreferences.setDouble(key, value);
   }
 }
