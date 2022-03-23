919d09293c0ba3458f356d97e8f00ec5ba1fa14e
saci zakaria <sacideveloppeur@gmail.com>
2022-03-23 12:08:56 +0100

    List<Widget>  bottomScreens

--- lib/modules/shop_app/cateogries/categories_screen.dart
+++ lib/modules/shop_app/cateogries/categories_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Center(
