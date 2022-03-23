f9b58e5c4a1aa3fd80223803d35700b7791c1709
saci zakaria <sacideveloppeur@gmail.com>
2022-03-23 11:18:49 +0100

    class ShopCubit

---
+++ lib/layout/shop_app/cubit/cubit.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:app_theme_mansour/layout/shop_app/cubit/states.dart';
+import 'package:bloc/bloc.dart';
+
+class ShopCubit extends Cubit<ShopStates>{
+  ShopCubit(ShopStates initialState) : super(initialState);
+  // ShopCubit() : super(ShopInitialState());
+
+}
