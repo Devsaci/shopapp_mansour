881c91ad943e180eadef4de397eb8ce1309cbc69
saci zakaria <sacideveloppeur@gmail.com>
2022-03-23 11:52:21 +0100

    'Products Screen',

--- lib/modules/shop_app/products/products_screen.dart
+++ lib/modules/shop_app/products/products_screen.dart
@@ -1,13 +1,17 @@
-
-
+import 'dart:html';
 
 import 'package:flutter/material.dart';
 
-class ProductsScreen  extends StatelessWidget {
-  const ProductsScreen ({Key? key}) : super(key: key);
+class ProductsScreen extends StatelessWidget {
+  const ProductsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text(
+        'Products Screen',
+        style: Theme.of(context).textTheme.bodyText1,
+      ),
+    );
   }
 }
