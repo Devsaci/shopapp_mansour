cb68d1f4cba7c72a53886d0c1cdf5770e68e0fcf
saci zakaria <sacideveloppeur@gmail.com>
2022-03-23 14:10:13 +0100

    BlocConsumer

--- lib/layout/shop_app/shop_layout.dart
+++ lib/layout/shop_app/shop_layout.dart
@@ -1,30 +1,35 @@
-
 import 'package:app_theme_mansour/modules/shop_app/login/shop_login_screen.dart';
 import 'package:app_theme_mansour/shared/components/components.dart';
 import 'package:app_theme_mansour/shared/network/local/cache_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ShopLayout extends StatelessWidget {
   const ShopLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Salla'),
-      ),
-      body: TextButton(
-        onPressed: () {
-          CacheHelper.removeData(
-            key: 'token',
-          ).then((value) {
-            if(value){
-              navigateAndFinish(context, ShopLoginScreen());
-            }
-          });
-        },
-        child: const Center(child: Text('SING OUT')),
-      ),
+    return BlocConsumer(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Salla'),
+          ),
+          body: TextButton(
+            onPressed: () {
+              CacheHelper.removeData(
+                key: 'token',
+              ).then((value) {
+                if (value) {
+                  navigateAndFinish(context, ShopLoginScreen());
+                }
+              });
+            },
+            child: const Center(child: Text('SING OUT')),
+          ),
+        );
+      },
     );
   }
 }
