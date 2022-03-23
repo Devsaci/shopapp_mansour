f86fcd1b28af51ff9621cf96935bc359b1b6f4a3
saci zakaria <sacideveloppeur@gmail.com>
2022-03-23 10:47:26 +0100

    107. Build Shop Layout [1]

--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -41,3 +41,18 @@
 // "token": "8aU6GtBrzqRXkt6PcaSWHy0AxDvyLcB9SfLBlEYvLjJXmoR2swiBlGAhf7NTGQG01bp4ks"
 // }
 // }
+//
+// {
+// "status": true,
+// "message": "تم تسجيل الدخول بنجاح",
+// "data": {
+// "id": 13012,
+// "name": "abdullah",
+// "email": "abdullah.ali@gmail.com",
+// "phone": "2514469853250",
+// "image": "https://student.valuxapps.com/storage/assets/defaults/user.jpg",
+// "points": 0,
+// "credit": 0,
+// "token": "NXiz5Kej0jd3ex34ycKQXp7ugQjo7F1LcPtobTw5fL9FOqifhdq449LVMaAiOjGH0CIAPp"
+// }
+// }
