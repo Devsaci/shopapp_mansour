fbb040d3e084eefb0a9662e4bc987fc70d99fe90
saci zakaria <sacideveloppeur@gmail.com>
2022-04-10 13:03:52 +0200

    ShopErrorHomeDataState

--- lib/layout/shop_app/cubit/states.dart
+++ lib/layout/shop_app/cubit/states.dart
@@ -7,3 +7,5 @@ class ShopChangeBottomNavState extends ShopStates {}
 class ShopLoadingHomeDataState extends ShopStates {}
 
 class ShopSuccessHomeDataState extends ShopStates {}
+
+class ShopErrorHomeDataState extends ShopStates {}
