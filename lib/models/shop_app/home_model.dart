8cbc2b45536f7e056cc250f6c1ec1b712de094c5
saci zakaria <sacideveloppeur@gmail.com>
2022-04-10 11:25:18 +0200

    class BannerModel {}

--- lib/models/shop_app/home_model.dart
+++ lib/models/shop_app/home_model.dart
@@ -1,9 +1,9 @@
 class HomeModel {
   bool? status;
   HomeDataModel? data;
+
 // named Constructor
-  HomeModel.fromJson(Map<String, dynamic> json)
-  {
+  HomeModel.fromJson(Map<String, dynamic> json) {
     status = json['status'];
     data = HomeDataModel.fromJson(json['data']);
   }
@@ -12,3 +12,5 @@ class HomeModel {
 class HomeDataModel {
   HomeDataModel.fromJson(Map<String, dynamic> json);
 }
+
+class BannerModel {}
