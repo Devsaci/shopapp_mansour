7f6af9340d723ad7741d911719afe52365cd388f
saci zakaria <sacideveloppeur@gmail.com>
2022-04-10 13:21:36 +0200

    rename categories

--- lib/layout/shop_app/cubit/cubit.dart
+++ lib/layout/shop_app/cubit/cubit.dart
@@ -1,9 +1,10 @@
 import 'package:app_theme_mansour/layout/shop_app/cubit/states.dart';
+import 'package:app_theme_mansour/shared/network/remote/dio_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../models/shop_app/home_model.dart';
-import '../../../modules/shop_app/cateogries/categories_screen.dart';
+import '../../../modules/shop_app/categories/categories_screen.dart';
 import '../../../modules/shop_app/favorites/favorites_screen.dart';
 import '../../../modules/shop_app/products/products_screen.dart';
 import '../../../modules/shop_app/settings/settings_screen.dart';
@@ -30,8 +31,8 @@ class ShopCubit extends Cubit<ShopStates> {
   }
 
   HomeModel? homeModel;
+
   void getHomeData() {
     emit(ShopLoadingHomeDataState());
-
   }
 }
