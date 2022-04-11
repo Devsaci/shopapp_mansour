85ac0d3da596ad514fa7bbda906c6f5b149e0cbe
saci zakaria <sacideveloppeur@gmail.com>
2022-04-11 23:20:49 +0200

    Follow Section 108 at 8:36

--- lib/layout/shop_app/cubit/cubit.dart
+++ lib/layout/shop_app/cubit/cubit.dart
@@ -44,7 +44,6 @@ class ShopCubit extends Cubit<ShopStates> {
       homeModel = HomeModel.fromJson(value.data);
       print(homeModel?.data?.banners[0].image);
       print('////******//////${homeModel?.status}////******/////');
-
       emit(ShopSuccessHomeDataState());
     }).catchError((error) {
       print(error.toString());
