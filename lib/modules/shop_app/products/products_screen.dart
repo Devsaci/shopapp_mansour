2304f8d8112dcb50ebde893094d1b0ee4eb78024
saci zakaria <sacideveloppeur@gmail.com>
2022-04-12 18:11:21 +0200

    BlocConsumer

--- lib/modules/shop_app/products/products_screen.dart
+++ lib/modules/shop_app/products/products_screen.dart
@@ -1,16 +1,18 @@
+import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Products Screen',
-        style: Theme.of(context).textTheme.bodyText1,
-      ),
+    return BlocConsumer(
+      listener: (BuildContext context, state) {},
+      builder: (BuildContext context, Object? state) {
+        return Text("data");
+      },
     );
   }
 }
