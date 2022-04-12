4b2fc4b1ca9cb95c124d78c585f5c9bb5cd79ab6
saci zakaria <sacideveloppeur@gmail.com>
2022-04-12 19:50:30 +0200

    carousel_slider: ^4.0.0 options: CarouselOptions

--- lib/modules/shop_app/products/products_screen.dart
+++ lib/modules/shop_app/products/products_screen.dart
@@ -34,7 +34,18 @@ class ProductsScreen extends StatelessWidget {
               width: double.infinity,
               fit: BoxFit.cover,
             ),).toList(),
-            options: CarouselOptions(),
+            options: CarouselOptions(
+              height: 200.0,
+              initialPage: 0,
+              viewportFraction: 1.0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              autoPlayAnimationDuration: const Duration(seconds: 1),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              scrollDirection: Axis.horizontal,
+            ),
           )
         ],
       );
