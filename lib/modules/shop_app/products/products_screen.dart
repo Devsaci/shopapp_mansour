671e61a53b25aeb18a28fc710845ccae554d4ddc
saci zakaria <sacideveloppeur@gmail.com>
2022-04-12 18:12:13 +0200

    <ShopCubit, ShopStates>

--- lib/modules/shop_app/products/products_screen.dart
+++ lib/modules/shop_app/products/products_screen.dart
@@ -3,12 +3,15 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../layout/shop_app/cubit/cubit.dart';
+import '../../../layout/shop_app/cubit/states.dart';
+
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer(
+    return BlocConsumer<ShopCubit, ShopStates>(
       listener: (BuildContext context, state) {},
       builder: (BuildContext context, Object? state) {
         return Text("data");
