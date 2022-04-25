3edf04c6a4994fa3f08d3b3500306b1f713be2d1
saci zakaria <sacideveloppeur@gmail.com>
2022-04-25 18:15:22 +0200

    late int currentPage;

--- lib/models/shop_app/categories_model.dart
+++ lib/models/shop_app/categories_model.dart
@@ -5,7 +5,10 @@ class CategoriesModel {
 // named Constructor
   CategoriesModel.fromJson(Map<String, dynamic> json) {
     status = json['status'];
+    data = (json['data']);
   }
 }
 
-class CategoriesDataModel {}
+class CategoriesDataModel {
+  late int currentPage;
+}
