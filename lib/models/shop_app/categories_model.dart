57938772d68cb57c4a3d504ea916027b7503c75b
saci zakaria <sacideveloppeur@gmail.com>
2022-04-25 22:06:09 +0200

    DataModel

--- lib/models/shop_app/categories_model.dart
+++ lib/models/shop_app/categories_model.dart
@@ -22,6 +22,13 @@ class CategoriesDataModel {
 }
 
 class DataModel {
+  int? id;
+  String? name;
+  String? image;
 
-  DataModel.fromJson(Map<String, dynamic> json);
+  DataModel.fromJson(Map<String, dynamic> json){
+    id = json['id'];
+    name = json['name'];
+    image = json['image'];
+  }
 }
