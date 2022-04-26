92a5c042a8ad66b87fefd300faab395637d36712
saci zakaria <sacideveloppeur@gmail.com>
2022-04-26 17:22:52 +0200

    Colors.black.withOpacity(0.7),

--- lib/modules/shop_app/products/products_screen.dart
+++ lib/modules/shop_app/products/products_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:app_theme_mansour/models/shop_app/home_model.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
@@ -54,17 +56,21 @@ class ProductsScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(
-                height: 10.0,
-                ),
+              height: 10.0,
+            ),
             Stack(
-              children:   [
-                Image(
-                  image: NetworkImage('https://student.valuxapps.com/storage/uploads/banners/1619472351ITAM5.3bb51c97376281.5ec3ca8c1e8c5.jpg'),
-                  height: 100.0,
-                  width: 100.0,
+              children: [
+                const Image(
+                  image: NetworkImage(
+                      'https://student.valuxapps.com/storage/uploads/banners/1619472351ITAM5.3bb51c97376281.5ec3ca8c1e8c5.jpg'),
+                  height: 150.0,
+                  width: 150.0,
                   fit: BoxFit.cover,
                 ),
-                Container(child: Text("Electronics")),
+                Container(
+                  color: Colors.black.withOpacity(0.7),
+                  child: const Text("Electronics", style: TextStyle(color: Colors.white),),
+                ),
               ],
             ),
             Container(
