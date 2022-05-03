6c7ffdfbfea6f97806703c4f028494f8734ed5ce
saci zakaria <sacideveloppeur@gmail.com>
2022-05-03 21:07:40 +0200

    return buildCatItem();

--- lib/modules/shop_app/categories/categories_screen.dart
+++ lib/modules/shop_app/categories/categories_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:html';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -8,36 +5,36 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Row(
-        children: const [
-          Image(
-            image: NetworkImage(
-              'https://student.valuxapps.com/storage/uploads/banners/1619472351ITAM5.3bb51c97376281.5ec3ca8c1e8c5.jpg',
-            ),
-            width: 120.0,
-            height: 120.0,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            width: 20.0,
-          ),
-          Text(
-            'model.name',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Spacer(),
-          Icon(
-            Icons.arrow_forward_ios,
-          ),
-        ],
-      ),
-    );
+    return buildCatItem();
   }
 
-  Widget buildCatItem( ) => Text('Padding');
+  Widget buildCatItem() => Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Row(
+      children: const [
+        Image(
+          image: NetworkImage(
+            'https://student.valuxapps.com/storage/uploads/banners/1619472351ITAM5.3bb51c97376281.5ec3ca8c1e8c5.jpg',
+          ),
+          width: 120.0,
+          height: 120.0,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        Text(
+          'model.name',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Spacer(),
+        Icon(
+          Icons.arrow_forward_ios,
+        ),
+      ],
+    ),
+  );
 }
