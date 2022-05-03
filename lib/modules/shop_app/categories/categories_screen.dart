63dddc62b3ecb474620c6e8bcc6f9350797af4b3
saci zakaria <sacideveloppeur@gmail.com>
2022-05-03 21:02:13 +0200

    Text / Spacer / Icons.arrow_forward_ios

--- lib/modules/shop_app/categories/categories_screen.dart
+++ lib/modules/shop_app/categories/categories_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -6,8 +7,29 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [
-        Image(image: NetworkImage('url'))
+      children: const [
+        Image(
+          image: NetworkImage(
+            'https://student.valuxapps.com/storage/uploads/banners/1619472351ITAM5.3bb51c97376281.5ec3ca8c1e8c5.jpg',
+          ),
+          width: 120.0,
+          height: 120.0,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        Text(
+          'model.name',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Spacer(),
+        Icon(
+          Icons.arrow_forward_ios,
+        ),
       ],
     );
   }
