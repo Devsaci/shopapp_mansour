c911a8b6d233a9bd49d26374ebb09b6e6033d6c0
saci zakaria <sacideveloppeur@gmail.com>
2022-05-03 21:03:43 +0200

    Padding

--- lib/modules/shop_app/categories/categories_screen.dart
+++ lib/modules/shop_app/categories/categories_screen.dart
@@ -6,31 +6,34 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: const [
-        Image(
-          image: NetworkImage(
-            'https://student.valuxapps.com/storage/uploads/banners/1619472351ITAM5.3bb51c97376281.5ec3ca8c1e8c5.jpg',
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: const [
+          Image(
+            image: NetworkImage(
+              'https://student.valuxapps.com/storage/uploads/banners/1619472351ITAM5.3bb51c97376281.5ec3ca8c1e8c5.jpg',
+            ),
+            width: 120.0,
+            height: 120.0,
+            fit: BoxFit.cover,
           ),
-          width: 120.0,
-          height: 120.0,
-          fit: BoxFit.cover,
-        ),
-        SizedBox(
-          width: 20.0,
-        ),
-        Text(
-          'model.name',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
+          SizedBox(
+            width: 20.0,
           ),
-        ),
-        Spacer(),
-        Icon(
-          Icons.arrow_forward_ios,
-        ),
-      ],
+          Text(
+            'model.name',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Spacer(),
+          Icon(
+            Icons.arrow_forward_ios,
+          ),
+        ],
+      ),
     );
   }
 }
